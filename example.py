1c644396bb893a741bff5f9b11b7e1bdc2ac0978
oberlav1 <victoria.oberlander@aalto.fi>
2020-06-04 11:19:07 +0300

    added subtraction function

--- example.py
+++ example.py
@@ -6,13 +6,12 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
+def subtract(a, b):
+    return a + b  # <--- fix this in step 8
 
 
-def divise(a, b):
-    return a / b
-
-
-# def test_divise():
-#     assert add(5, 2) == 2.5
+# uncomment the following test in step 5
+def test_subtract():
+    assert subtract(2, 3) == -1
 
 
