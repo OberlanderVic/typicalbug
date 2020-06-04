ecd9bc816a165c5adf0365e3678e55bfcc1d8dbe
oberlav1 <victoria.oberlander@aalto.fi>
2020-06-04 11:11:09 +0300

    add & divise functions

---
+++ example.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+
+def divise(a, b):
+    return a / b
+
+
+# def test_divise():
+#     assert add(5, 2) == 2.5
+
+
